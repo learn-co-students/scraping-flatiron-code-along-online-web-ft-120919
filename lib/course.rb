5e6a30efde17338754c2a9fd758b4f05ee569eca
Mikael Kaya <kayaswork@gmail.com>
2020-01-18 23:07:38 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,21 @@
-
-
+class Course
+ 
+  attr_accessor :title, :schedule, :description
+ 
+  @@all = []
+ 
+  def initialize
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
+ 
+ doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+end
